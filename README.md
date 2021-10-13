02d9252750171afcaa535636f37837fca449d156
Nero <resqnetm@gmail.com>
2021-10-13 22:11:59 +0300

    Update doc

--- README.md
+++ README.md
@@ -5,6 +5,26 @@ Specific hashmap that works fine with monotonic integer values starting from zer
 
 - Optimize division with bitwise operations
 
-# API Doc
+# Public API Doc
 
+```c
+/* Initialize lilmap with custom settings */
+void lilmap_init_custom(LilMap *map, size_t capacity,
+                        lilmap_float load_factor);
 
+/* Insert key-value pair to map */
+void lilmap_set(LilMap *map, lilmap_int key,
+                void *value);
+
+/* Lookup for key in map */
+void* lilmap_lookup(LilMap *map, lilmap_int key);
+
+/* Erase key from map */
+void lilmap_erase(LilMap *map, lilmap_int key);
+
+/* Initialize lilmap with default settings */
+inline static void lilmap_init(LilMap *map);
+
+/* Free lilmap's memory */
+inline static void lilmap_free(LilMap *map)
+```
--- src/lilmap/lilmap.h
+++ src/lilmap/lilmap.h
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 #define LM_TOMBSTONE ((void *)0xDEAD)
-#define LM_DEFAULT_CAPACITY 1
+#define LM_DEFAULT_CAPACITY 64
 #define LM_DEFAULT_LOAD_FACTOR 0.75f
 
 #define LM_MAX_PROBES 128u
@@ -34,18 +34,25 @@ typedef struct {
     lilmap_float load_factor;
 } LilMap;
 
-
+/* Initialize lilmap with custom settings */
 void lilmap_init_custom(LilMap *map, size_t capacity,
                         lilmap_float load_factor);
 
+/* Insert key-value pair to map */
 void lilmap_set(LilMap *map, lilmap_int key,
                 void *value);
+
+/* Lookup for key in map */
 void* lilmap_lookup(LilMap *map, lilmap_int key);
+
+/* Erase key from map */
 void lilmap_erase(LilMap *map, lilmap_int key);
 
+/* Grow hash map by doubling its capacity */
 void lilmap_grow(LilMap *map, lilmap_int key,
                  void *value);
 
+/* Get bucket index from map */
 inline static
 size_t lilmap_index(LilMap *map, lilmap_int key) {
     size_t index = key % map->buckets_allocated;
@@ -53,22 +60,26 @@ size_t lilmap_index(LilMap *map, lilmap_int key) {
     return index - (index == map->buckets_allocated);
 }
 
+/* Calculate load factor; WARNING: floating point exception may be thrown */
 inline static
 lilmap_float lilmap_load_factor(LilMap *map, size_t additional) {
     return ((lilmap_float)(map->buckets_used + additional)) / ((lilmap_float)map->buckets_allocated);
 }
 
+/* Initialize lilmap with default settings */
 inline static
 void lilmap_init(LilMap *map) {
     lilmap_init_custom(map, LM_DEFAULT_CAPACITY,
                        LM_DEFAULT_LOAD_FACTOR);
 }
 
+/* Check whether bucket is used or not */
 inline static
 bool lilmap_is_occupied(struct LilMapBucket *bucket) {
     return (bucket->value != NULL) && (bucket->value != LM_TOMBSTONE);
 }
 
+/* Free map */
 inline static
 void lilmap_free(LilMap *map) {
     free(map->buckets);
